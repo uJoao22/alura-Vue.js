68b579df8225bff039f99cf2d4f390ad6ab95345
joaoBotelho22 <joaopedro@saudeagora.com.vc>
2021-12-16 17:17:27 -0300

    Aplicando animação usando a tag transition do Vue

--- alurapic/src/components/shared/painel/Painel.vue
+++ alurapic/src/components/shared/painel/Painel.vue
@@ -2,13 +2,16 @@
         <div class="painel">
             <!-- QUando eu clicar duas vezes no h2 abaio ele irá trocar o estado da propriedade visivael para seu oposto, para mostrar o painel ou ocultar -->
             <!-- Usando o atalho de 'v-on:' que é usar '@' -->
-          <h2 class="painel-titulo" @dblclick="visivel = !visivel">{{ titulo }}</h2>
+            <h2 class="painel-titulo" @dblclick="visivel = !visivel">{{ titulo }}</h2>
 
-          <!-- Usando slot no lugar da div para dizer onde a imagem inserida dentro de 'meu-painel' será colocar aqui no componente Paiel -->
-          <!-- Aplicando o v-show que faz com que o elemento ganhe um "display none", mas para isso o v-show tem que estar aplicado em uma div pai do slot -->
-          <div class="painel-corpo" v-show="visivel">
-              <slot></slot>
-          </div>
+            <!-- Aplicando o transition para aplicar determinadas classes em determinnado estado do painel, para faciliar no processo de animação -->
+            <transition name="painel-fade">
+                <!-- Aplicando o v-show que faz com que o elemento ganhe um "display none", mas para isso o v-show tem que estar aplicado em uma div pai do slot -->
+                <div class="painel-corpo" v-show="visivel">
+                    <!-- Usando slot no lugar da div para dizer onde a imagem inserida dentro de 'meu-painel' será colocar aqui no componente Paiel -->
+                    <slot></slot>
+                </div>
+            </transition>
         </div>
 </template>
 
@@ -28,37 +31,46 @@ export default {
 
 // Defiido um Escopo para o estilo, para que apenas os itens dele sofressem a alteração do css
 <style scoped>
-   .painel {
-    padding: 0 auto;
-    border: solid 2px grey;
-    display: inline-block;
-    margin: 5px;
-    box-shadow: 5px 5px 10px grey;
-    width: 200px;
-    vertical-align: top;
-    text-align: center;
-  }
+    .painel {
+        padding: 0 auto;
+        border: solid 2px grey;
+        display: inline-block;
+        margin: 5px;
+        box-shadow: 5px 5px 10px grey;
+        width: 200px;
+        vertical-align: top;
+        text-align: center;
+    }
 
-  .painel-corpo{
-    height: 230px;
-    display: flex;
-    align-items: center;
-  }
+    .painel-corpo{
+        height: 230px;
+        display: flex;
+        align-items: center;
+    }
 
-  .painel .painel-titulo {
-    border: solid 2px;
-    background: lightblue;
-    margin: 0 0 0px 0;
-    padding: 10px;
-    height: 50px;
-    text-transform: uppercase;
-    display: flex;
-    justify-content: center;
-    align-items: center
-  }
+    .painel .painel-titulo {
+        border: solid 2px;
+        background: lightblue;
+        margin: 0 0 0px 0;
+        padding: 10px;
+        height: 50px;
+        text-transform: uppercase;
+        display: flex;
+        justify-content: center;
+        align-items: center
+    }
 
-  .painel-titulo{
-    cursor: pointer;
-    user-select: none;
-  }
+    .painel-titulo{
+        cursor: pointer;
+        user-select: none;
+    }
+
+    .painel-fade-enter, .painel-fade-leave-active{
+        opacity: 0;
+    }
+
+    .painel-fade-enter-active, .painel-fade-leave-active{
+        transition: opacity .5s
+    }
+
 </style>
