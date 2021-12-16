368750b2bc9e040a63ed282ea509aeaa16bc462c
joaoBotelho22 <joaopedro@saudeagora.com.vc>
2021-12-16 14:47:04 -0300

    Usando promises e o metodo  na função hooks created

--- alurapic/src/App.vue
+++ alurapic/src/App.vue
@@ -6,6 +6,7 @@
     <!-- Dentro de atributos não se pode usar interpolação, devo usar o v-bind antes do atributo, mas existe um atalho que é usar apenas ':' antes do atributo -->
     <ul>
       <!-- Usando a diretiva v-for para iterar um array, usando a ':key' para evitar o erro de sintaxe -->
+      <!-- Os dados recebidos da API pela promessa do $http são iterados no li -->
       <li v-for="(foto, index) of fotos" :key="index">
         <img :src="foto.url" :alt="foto.titulo">
       </li>
@@ -17,20 +18,24 @@
 <script>
 export default {
 
-  data(){
+  data(){ //É na função data que fornecemos os dados que o template precisa
 
     return {
       titulo: "Alurapic",
-      fotos: [{
-        url: "https://tudovet.com.br/blog/wp-content/uploads/2018/11/expectativa-de-vida-cachorro-845x423.jpg",
-        titulo: "cachorro"
-      },
-      {
-        url: "https://www.petz.com.br/blog/wp-content/uploads/2021/02/raca-de-cachorro-grande-2-1280x720.jpg  ",
-        titulo: "Cachorrão"
-      }]
+      fotos: []
     }
 
+  },
+
+  created(){ //Usando da função created para eecutar um bloco de código toda vez que o componente é criado
+    //O componente '$http' vem do modulo VueResource
+    //Fazendo uma requisição dos dados do determinado endereço, essa requisição irá me retornar uma promise
+    let promise = this.$http.get('http://localhost:3000/v1/fotos')
+
+    //Acessando os dados retornados pela promesa em caso de sucesso, convertendo os dados para JSON que me retorna uma promesa, e se essa promesa retornar sucesso, o array fotos criado em data recebe os dados da promesa, se der erro irá exibir o erro no connsole
+    promise
+      .then(res => res.json())
+      .then(fotos => this.fotos = fotos, err => console.log(err))
   }
 }
 </script>
