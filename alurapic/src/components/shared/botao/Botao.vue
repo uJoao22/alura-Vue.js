96dce097c14bbea7c6b49cb78f6875ffa2ae3973
joaoBotelho22 <joaopedro@saudeagora.com.vc>
2021-12-17 12:42:36 -0300

    Validando propriedades

--- alurapic/src/components/shared/botao/Botao.vue
+++ alurapic/src/components/shared/botao/Botao.vue
@@ -5,8 +5,20 @@
 </template>
 
 <script>
+['tipo', 'rotulo', 'confirmacao', 'estilo']
 export default {
-    props: ['tipo', 'rotulo', 'confirmacao', 'estilo'],
+    props: {
+        tipo: {
+            required: true,
+            type: String
+        },
+        rotulo: {
+            required: true,
+            type: String
+        },
+        confirmacao: Boolean,
+        estilo: String
+    },
 
     methods: {
         disparaAcao(){
